7fe63c65821aafb136e53c96bd1b34416217b39e
jabed366 <jabedhasan231@gmail.com>
2022-06-15 20:09:13 +0600

    File send to media folder

--- Image upload Processing/server.js
+++ Image upload Processing/server.js
@@ -4,6 +4,12 @@ const app=express();
 const colors=require('colors')
 const connectMongoDB=require('./config/db')
 
+//for image uploaded
+const multer=require('multer')
+const upload=multer({
+    dest : './media/user'
+})
+
 //Connect mongo DB init
 connectMongoDB();
   
@@ -13,6 +19,18 @@ const PORT =process.env.SERVER_PORT;
 //server body init
 app.use(express.json())
 app.use(express.urlencoded({extended :false}))
+
+
+
+//Photo Upload
+
+app.post('/upload',upload.single('photo') ,(req,res) =>{
+    res.send("File send uploaded")
+})
+
+
+
+
 //students route Use
 app.use('/api/students',require('./routes/students'))
 
